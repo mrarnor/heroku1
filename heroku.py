498425db2d85fc70b6da832524a31753a853daf1
mrarnor <31922780+mrarnor@users.noreply.github.com>
2018-09-03 17:02:57 +0000

    Add files via upload

--- heroku.py
+++ heroku.py
@@ -1,12 +1,16 @@
 from sys import argv
 
+
+import bottle
 from bottle import *
+bottle.debug(True)
 
 @route("/")
 def index():
 	return """
-<h2>heroku<h2>
-"""
+	<h2>heroku<h2>
+	
+	"""
 
 
 bottle.run(host='0.0.0.0', port=argv[1])
